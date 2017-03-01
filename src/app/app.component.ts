795bc90440c1a526919cb01f3d19969d2b1898c5
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-03-01 20:31:37 -0300

    Setting to always open on the login page unless the user is already logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar, Splashscreen, NativeStorage } from 'ionic-native';
 
+import { ChatPage } from './../pages/chat/chat';
 import { LoginPage } from '../pages/login/login';
-
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = LoginPage;
-
+  @ViewChild(Nav) nav: Nav;
+  rootPage = LoginPage ;
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      let env = this;
+
+      NativeStorage.getItem('user')
+        .then(data => {
+          env.nav.push(ChatPage);
+          Splashscreen.hide();
+        }, error => {
+          console.log(error)
+          env.nav.push(LoginPage);
+          Splashscreen.hide();
+
+        });
+
       StatusBar.styleDefault();
-      Splashscreen.hide();
     });
   }
 }
