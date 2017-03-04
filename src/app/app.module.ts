e564530fef4bc49c8fabcc6b0cbc31a7ee30b6e6
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-03-04 20:16:46 -0300

    Deleting var

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 import { LoginPage } from '../pages/login/login';
 import { ChatPage } from './../pages/chat/chat';
